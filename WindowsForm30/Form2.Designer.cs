802e26b0a05b1bdb0eac1dae7d1a546a8a8708ec
Student <Student@DESKTOP-JOKA8G8>
2018-11-30 12:04:07 +0630

    Add project files.

---
+++ WindowsForm30/Form2.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace WindowsForm30
+{
+	partial class Form2
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.button3 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 33);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(40, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "ID_NO";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 62);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(38, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "NAME";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 88);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(28, 13);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "SEC";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 114);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(52, 13);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "BRANCH";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(9, 140);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(96, 13);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "PROFILE_PHOTO";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(12, 163);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(30, 13);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "DOB";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(123, 33);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(121, 20);
+			this.textBox1.TabIndex = 6;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(123, 59);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(121, 20);
+			this.textBox2.TabIndex = 7;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(123, 85);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(121, 20);
+			this.textBox3.TabIndex = 8;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(123, 111);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(121, 20);
+			this.textBox4.TabIndex = 9;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(123, 137);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(121, 20);
+			this.textBox5.TabIndex = 10;
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+			this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+			this.dateTimePicker1.Location = new System.Drawing.Point(123, 163);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(121, 20);
+			this.dateTimePicker1.TabIndex = 11;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(264, 33);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(119, 23);
+			this.button1.TabIndex = 12;
+			this.button1.Text = "SAVE";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(462, 130);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(119, 23);
+			this.button2.TabIndex = 13;
+			this.button2.Text = "SHOW";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(611, 33);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(161, 143);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 14;
+			this.pictureBox1.TabStop = false;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(133, 205);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(617, 220);
+			this.dataGridView1.TabIndex = 15;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(264, 137);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(119, 23);
+			this.button3.TabIndex = 16;
+			this.button3.Text = "SELECT";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(617, 9);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(52, 13);
+			this.label7.TabIndex = 17;
+			this.label7.Text = "PROFILE";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(462, 55);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(121, 20);
+			this.textBox6.TabIndex = 18;
+			// 
+			// Form2
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form2";
+			this.Text = "REGISTRATION_FORM";
+			this.Load += new System.EventHandler(this.Form2_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox6;
+	}
+}
---
+++ WindowsForm30/Form2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace WindowsForm30
+{
+	public partial class Form2 : Form
+	{
+		SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Student\source\repos\WindowsForm30\WindowsForm30\Database1.mdf;Integrated Security=True");
+		public Form2()
+		{
+			InitializeComponent();
+		}
+
+		private void Form2_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			con.Open();
+			SqlCommand cmd = con.CreateCommand();
+			cmd.CommandType = CommandType.Text;
+			cmd.CommandText = "insert into Register values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" +textBox4.Text+"','" +textBox5.Text+"','" + dateTimePicker1.Text + "')";
+			cmd.ExecuteNonQuery();
+
+			MessageBox.Show("saved");
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				textBox5.Text = openFileDialog1.FileName;
+
+				textBox5.Text = Path.GetFileName(openFileDialog1.FileName);
+				pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+
+			}
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
